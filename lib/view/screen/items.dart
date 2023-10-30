2056e8597eecdbc3e2800a32e3e7e5329870f228
Dalia Karem <daliakarem0027@gmail.com>
2023-10-30 13:34:41 +0200

    Edit Page Categories

--- lib/view/screen/items.dart
+++ lib/view/screen/items.dart
@@ -21,7 +21,61 @@ class Items extends StatelessWidget {
               height: 20,
             ),
             ListCategoriesItems(),
-          ],
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 12,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(childAspectRatio: 0.7,crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                // Item builder function
+                return Container(
+
+                  child: InkWell(
+                    onTap: () {
+                      print("Clicked");
+                    },
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Stack(
+                            children: [
+                              Container(
+                                 padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+                                // margin: EdgeInsets.symmetric(horizontal: 10),
+                                child: Image.network(
+                                  "https://img.fruugo.com/product/9/89/535273899_max.jpg",
+                                  height: 130,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Positioned(
+                                top: -5,
+                                right: 2,
+                                child: IconButton(
+                                  icon: Icon(Icons.favorite, color: Colors.red),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text("Smart Phone",style: TextStyle(fontWeight: FontWeight.bold,),),
+                          Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 18.0),
+                                child: Text("\$22"),
+                              ),
+                              Spacer(),
+                              IconButton(onPressed: (){}, icon: Icon(Icons.add_shopping_cart))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            )          ],
         ),
       ),
     );
